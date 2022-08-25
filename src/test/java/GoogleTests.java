c474b2d121b62ccf7ddb9d7a87f946d0ceb5774c
nadin <ns@pimpay.ru>
2022-08-25 11:56:54 +0300

    added googleTests/selenideSearchTest

---
+++ src/test/java/GoogleTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTests
+{
+
+    @Test
+    void selenideSearchTest()
+    {
+        //открыть google
+        open("https://www.google.com/");
+        //ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+        //проверить, что Selenide есть в результатах поиска
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
