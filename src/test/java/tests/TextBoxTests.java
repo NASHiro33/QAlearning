970041b30328cbc7fa263dd74814d1f668e65350
nadin <ns@pimpay.ru>
2022-08-27 12:17:06 +0300

    added checkDataInOutputTest

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+    @Test
+    void checkDataInOutputTest()
+    {
+        String name = "Nadin";
+        String email = "ns@olol.ru";
+        String currentAddress = "Moscow-city";
+        String permanentAddress = "2279 3rd Ave";
+        open("https://demoqa.com/text-box");
+
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue(email);
+        $("[id=currentAddress]").setValue(currentAddress);
+        $("[id=permanentAddress]").setValue(permanentAddress);
+        $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text(email), text(name), text(currentAddress), text(permanentAddress));
+        sleep(4000);
+    }
+}
