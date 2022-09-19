7df4464647d7b34bf4a9057384036fe972b16080
nadin <ns@pimpay.ru>
2022-09-20 00:02:46 +0300

    added cycle and hashMap for example

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -4,10 +4,13 @@ package tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -15,23 +18,45 @@ import org.openqa.selenium.WebElement;
 
 import java.io.File;
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
+    //todo убрать мапу и переменные в отдельный файл, а то мусорка
+    //todo привести в порядок импорты
+    static Faker faker = new Faker();
+    static String
+            firstName = faker.name().firstName(),
+            lastName  = faker.name().lastName(),
+            email     = faker.internet().emailAddress(),
+            phone     = faker.number().digits(10),
+            address   = faker.address().fullAddress();
+    static Map<String, String> expectedData = new HashMap<>()
+    {{
+        put("Student Name", firstName + " " + lastName);
+        put("Student Email", email);
+        put("Gender", "Male");
+        put("Mobile", phone);
+        put("Date of Birth", "15 February,1988");
+        put("Subjects", "Arts, Chemistry, Maths");
+        put("Hobbies", "Reading, Music");
+        put("Picture", "Junit5Annotations.java");
+        put("Address", address);
+        put("State And City", "NCR Delhi");
+    }};
 
     @Test
     void checkPracticeFormOutput() {
-        Faker faker = new Faker();
 
-        String firstName = faker.name().firstName();
-        String lastName  = faker.name().lastName();
-        String email     = faker.internet().emailAddress();
-        String phone     = faker.number().digits(10);
-        String address   = faker.address().fullAddress();
+//        String[] hobbies   = {"Sports", "Music", "Reading"};
+//        Stream<String> hobbie = Arrays.stream(hobbies).findAny().toString(); - попытка поработать со stream
 
         open("https://demoqa.com/automation-practice-form");
         $("[class='main-header']").shouldHave(text("Practice Form"));
@@ -66,20 +91,31 @@ public class PracticeFormTests {
         $("[id='submit']").pressEnter();
 
         //asserts
+        //todo так мы каждый раз обращаемся к сайту, можно переделать на js, но там ебок
         $("[id='example-modal-sizes-title-lg']").shouldHave(text("Thanks for submitting the form"));
-        $("[class='table-responsive']").shouldHave(
-                text(firstName),
-                text(lastName),
-                text(email),
-                text("Male"),
-                text(phone),
-                text("15 February,1988"),
-                text("Arts, Chemistry, Maths"),
-                text("Reading, Music"),
-                text("Junit5Annotations.java"),
-                text(address),
-                text("NCR Delhi")
-        );
+//        $("[class='table-responsive']").shouldHave(
+//                text(firstName),
+//                text(lastName),
+//                text(email),
+//                text("Male"),
+//                text(phone),
+//                text("15 February,1988"),
+//                text("Arts, Chemistry, Maths"),
+//                text("Reading, Music"),
+//                text("Junit5Annotations.java"),
+//                text(address),
+//                text("NCR Delhi")
+//          );
+        //Вариант проверки через цикл,
+        SoftAssertions softly = new SoftAssertions();
+       for(SelenideElement element: $$(".table-responsive"))
+        {
+            String key = element.$("td").getText();
+            String actualValue = element.$("td", 1).getText();
+            String expectedValue = expectedData.get(key);
+            softly.assertThat(actualValue).isEqualTo(expectedValue);
+        }
+       softly.assertAll();
     }
 
     @Test
